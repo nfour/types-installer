06a2024c76de1043e7e2ecd58fe4f15c784fcac4
nfour <novus.nfour@gmail.com>
2017-11-27 11:02:52 +1000

    jest

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,11 @@ module.exports = {
   "transform": { ".(ts|tsx)": "<rootDir>/node_modules/ts-jest/preprocessor.js" },
   "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
   "moduleFileExtensions": [ "ts", "tsx", "js", "json" ],
+  "coverageDirectory": ".coverage",
+  "coverageReporters": ['text', 'text-summary'],
+  "coverageThreshold": {
+    "global": { lines: 60 }
+  },
   "testPathIgnorePatterns": [
     "/node_modules/",
     "./dist"
