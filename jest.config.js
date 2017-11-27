032c68c090cab90d8b8ed4597d0304f78743aa5f
nfour <novus.nfour@gmail.com>
2017-11-27 11:00:47 +1000

    uplift

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
-{
+module.exports = {
   "verbose": true,
   "transform": { ".(ts|tsx)": "<rootDir>/node_modules/ts-jest/preprocessor.js" },
   "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
-  "moduleFileExtensions": [ "ts", "tsx", "js" ],
+  "moduleFileExtensions": [ "ts", "tsx", "js", "json" ],
   "testPathIgnorePatterns": [
     "/node_modules/",
     "./dist"
